7bbc82a73b8e1dec15d985be9e8e8793c0fa993f
profhamachi <ahamachi@pasadena.edu>
2023-08-21 21:10:33 -0700

    added 4-2.py

--- apps/deleteme.py
+++
@@ -1,2 +0,0 @@
-print("this is a test...you may delete this file. thanks.")
-print("this is another...")
---
+++ programming_exercises/4-2.py
@@ -0,0 +1,14 @@
+# Variables
+caloriesPerMinute = 4.2
+
+# Declare variables for the number of calories burned and the number of minutes.
+caloriesBurned = 0.0
+minutes = 0
+
+print("Minutes\t\tCalories Burned")
+print("-------------------------------")
+
+# Execute the for loop to display calories burned.
+for minutes in range(10, 31, 5):
+    caloriesBurned = caloriesPerMinute * minutes
+    print(minutes, "\t\t", caloriesBurned)
