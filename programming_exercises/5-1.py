6996a153ea760597d1a60c6f70430fa8f3b91a8c
profhamachi <ahamachi@pasadena.edu>
2023-08-28 19:15:57 -0700

    added programming exercise

---
+++ programming_exercises/5-1.py
@@ -0,0 +1,7 @@
+kilometers = float(input("Enter number of kilometers to convert to miles: "))
+
+conversion = 0.6214
+
+miles = kilometers * conversion
+
+print(f"{kilometers} kilometers is equal to {miles} miles")
